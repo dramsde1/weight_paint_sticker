66603574df25976d98f83056a627e19e2ce17b2f
dramsde1 <bluejaysramsden@gmail.com>
2024-04-02 00:33:23 -0400

    need to make the stamp thing

--- create_stamp.py
+++ create_stamp.py
@@ -3,14 +3,15 @@ The assumption is that you are creating a stamp for the same bone on a duplicate
 
 SETUP
 0) Pick the source and target armatures
+1) Assume the target armature is already parented to the target mesh
 
 GET WEIGHTS
 1) get the weights for each vertex in a vertex groups for a single bone 
 2) pick a central vertex in the vertex group to which all other vertices will translate/rotate in relation to 
 
 TRANSFER TO NEW ARMATURE
-3) estimate where the vertex group weights should go on the new mesh
-4) create the ability to move weights all at the same time on the new mesh
+3) move weights all at the same time to the target bone (retarget)
+4) allow on mouse movement to translate the whole weight painting map for a bone
 
 """
 import bpy
@@ -26,80 +27,79 @@ def get_mesh_from_armature(armature_name):
         return mesh_object
 
 
-def get_3d_center(source_object):
-    vertex_dict = {}
-    # Get the mesh data
-    mesh = source_object.data
 
-    total = mathutils.Vector((0, 0, 0))
-    count = 0
-    # Iterate through all vertices
-    for vertex in mesh.vertices:
-        total += vertex.co
-        count += 1
-
-    center = total / count
-    return center
-
-def find_nearest_vertex(center, source_object)
-    mesh = source_object.data
-    min_distance = 1000000
-    min_vertex = None
-    for vertex in mesh.vertices:
-        distance = calculate_distance(center, vertex.co)
-        if distance < min_distance:
-            min_distance = distance
-            min_vertex = vertex
-    return min_vertex
-
-
-def calculate_distance(vertex1, vertex2):
-    # Calculate the distance between the two points
-    return (vertex2 - vertex1).length
-
-
-def organize_vertex_group_info(mesh_object):
-    vertex_group_dict = {}
-    mesh = mesh_object.data
-    #Iterate through all vertices
-    for vertex in mesh.vertices:
-        for group_index, weight in vertex.groups:
-            vertex_group_dict[group_index] = vertex
-
-    return vertex_group_dict
-
-def estimate_target_vertex_group(source_armature_name):
-    #void return, just transfer weights to target vertex group
-
-    #source_armature = bpy.data.objects[source_armature_name]
-    #vertex_groups = source_armature.vertex_groups
-
-    source_object = get_mesh_from_armature(source_armature_name)
-    vertex_group_dict = organize_vertex_group_info(source_object)
-    center = get_3d_center(source_object)
-    #this center vertex is going to move with your mouse
-    center_vertex = find_nearest_vertex(center, source_object)
-    #now you need to estimate where this center vertex would be on the target mesh
-    #get bone name
-    #get 3d cor of bone center
-    # calculate distance between bone and center vertex
-    # find the vertex on the target mesh that best matches up with that distance
-    # map the source weights to the target mesh, centering on that newfound center vertex
+#1) get the weights for each vertex in a vertex groups for a single bone 
+def estimate_target_vertex(source_vertex):
+    
 
 
 
+#2) pick a central vertex in the vertex group to which all other vertices will translate/rotate in relation to 
+def get_center_of_surface_area():
 
 
 
+def is_in_vertex_group(vert_index, vert_group):
+      return vert_group.weight(vert_index) > 0
 
+
+#4) allow on mouse movement to translate the whole weight painting map for a bone
+def transfer_weights(source_armature_name, target_armature_name, source_mesh_name, target_mesh_name):
+
+    # Switch to object mode NOTE: need to figure out why I need to do this
+    bpy.ops.object.mode_set(mode='OBJECT')
+
+    #loop through source bones / target ones will be the same names
+    for source_vertex_group in bpy.data.objects[source_armature_name].pose.bones:
+        # make sure vertex group name is in mesh list of vertex groups
+        if source_vertex_group in bpy.data.objects[source_mesh_name].vertex_groups:
+
+            #first check if target_vertex_group already exists
+            target_vertex_group = bpy.data.objects[target_mesh_name].vertex_groups.get(source_vertex_group)
+
+            if target_vertex_group is None:
+                target_vertex_group = bpy.data.objects[target_mesh_name].vertex_groups.new(name=source_vertex_group)
             
+            #go through source mesh vertices to get weights for each vertex in source vertex group
+            mesh_data = bpy.data.objects[source_mesh_name].data
+
+    
+            # Loop through all vertices
+            for v in mesh_data.vertices:
+                # Get the weight of the vertex in the source group
+                try:
+                    #check if the vertex is in the source vertex group
+                    # a single vertex can belong to multiple vertex groups
+                    if is_in_vertex_group(v.index, source_vertex_group):
+                        # Assign the weight to the target group
+                        weight = source_vertex_group.weight(v.index)
+
+                        """
+                            May want to cache all of the weights somehow, then have a while loop run while you use mouse events to decide where to translate the weights stamp and then have a 
+                            key to both place the map and jump out of the while loop
+                        """
 
 
 
 
+                        target_vertex = estimate_target_vertex(v)
+                        if target_vertex:
+                            target_vertex_group.add([target_vertex.index], weight, 'REPLACE')
+                except RuntimeError as e:
+                    print(e)
+
+            print(f"Vertex group weights transferred from {source_bone_name} to {target_bone_name}")
+        else:
+            print(f"Vertex group '{source_bone_name}' not found.")
+    else:
+        print("Source or target bone not found.")
+
+
+
+# use an empty object to rotate and translate weight paint stamp
 
 
 
 
-
+def rotate_weights():
 
